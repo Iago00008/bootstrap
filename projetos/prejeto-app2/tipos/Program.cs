fe6e6527f724fc23a8fa119b4d82d1e54ea9e450
Iago00008 <iagofazevedo.08@gmail.com>
2025-03-27 10:47:19 -0300

    ola

---
+++ projetos/prejeto-app2/tipos/Program.cs
@@ -0,0 +1,41 @@
+﻿class program
+{
+    public static void Main()
+    {
+
+        //tipos numéricos 
+        //numéro do tipo inteiro
+        int numeroInt = 104;
+
+
+        // float - numeros com pontos flutuantes 
+        //(decimais)
+        float numeroFloat = 1.0f;
+
+
+        //double - numeros com pontos flutuantes 
+        //(decimais) - com precisão até 
+
+        double numeroDouble = 101.9545;
+        double numeroDouble2 = 101;
+
+        //decimal aom alte precisão
+        decimal numeroDecimal = (decimal)10000.32334434;
+
+        //tipos textuais
+
+        //caractere - valores com no max. 1 poscão
+        char caractere = 'I';
+        char caractere2 = (char)9;
+        char caractere3 = '9';
+
+        //string
+        //uso de aspas duplas 
+        string texto = "Iago Felipe Azevedo Ribeiro ";
+        string texto2 = "A";
+
+              
+
+
+    }
+}
