274563fb525cb8b6f75c92fe0c79ca5273a3c72f
Iago00008 <iagofazevedo.08@gmail.com>
2025-03-27 11:24:59 -0300

    pronto

---
+++ projetos/prejeto-app2/projeto-idade/Program.cs
@@ -0,0 +1,32 @@
+﻿//Receber o ano de nascimento da pessoa 
+
+//receber o ano atual do usuario
+//callcular a idade da pessoa 
+//mostra a idade da pessoa na tela
+class program
+{
+    public static void Main()
+    {
+        int num1, num2;
+
+
+        Console.WriteLine("Informe sue ano de nascimento:");
+
+        num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Informe o ano atual: ");
+
+        num2 = Convert.ToInt32(Console.ReadLine());
+
+        int subtracao = num2 - num1;
+
+        Console.WriteLine($"subtração: {subtracao}");
+
+
+
+
+
+
+
+    }
+}
